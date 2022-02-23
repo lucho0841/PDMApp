f0ae6d3ea026f31e5a569d5121b78dde92bb6d33
Luis <ingluiss@bythewave.co>
2022-02-22 19:47:49 -0500

    clase 2, hello world

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.myapplication
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import com.example.myapplication.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val boton = findViewById<Button>(R.id.btnNext)
+        boton.setOnClickListener{
+            val intent = Intent(this, second_Activity::class.java)
+            startActivity(intent)
+        }
+    }
+
+
+
+
+
+}
